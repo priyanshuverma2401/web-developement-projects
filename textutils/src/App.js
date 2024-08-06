d11ef61b192219a41d6a3988266a5678f402c3c9
priyanshu <priyanshukumarv984@gmail.com>
2024-08-07 00:44:13 +0530

    removed some unneccessary comments from app.js file.

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm"
-// import About from "./components/About"
+
 
 function App() { 
   return (
     <>
       <Navbar title="TextEdits"/>
-      {/* <About/> */}
+      
       <div className="container" >
         <TextForm heading="Enter The Text To analyze" title="Your Text"/>
       </div>
