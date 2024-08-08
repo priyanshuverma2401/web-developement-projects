a59b64635b147ebc00c20e7b5de30fe1a0493f32
priyanshu <priyanshukumarv984@gmail.com>
2024-08-09 01:45:59 +0530

    modified contact.js

---
+++ textutils/src/components/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+     <iframe src="https://www.linkedin.com/in/priyanshu-kumar-verma/" title="this is my linkdin handle" frameborder="0" height="500px" width="100%" style={{border : "none"}} ></iframe>
+    </div>
+  )
+}
